9eb3ae4ed46896c18c2bf46a32d8467a3433ef4b
Rafael Barros <rafaelbarros.df@gmail.com>
2021-07-15 17:49:15 -0300

    :construction: Implementando incluir tarefas

--- to-do-list/js/script.js
+++ to-do-list/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     new Vue({
       el: '#app',
       data: {
+          errorMessage: null,
             toDoList: [
                 { text: 'Tarefa 01', completed: false },
                 { text: 'Tarefa 02', completed: true }
@@ -10,7 +11,13 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         methods: {
             save() {
+                this.errorMessage = null
+                if (!this.toDo) {
+                    this.errorMessage = 'Digite o texto da tarefa!'
+                    return
+                }
                 this.toDoList.push({text: this.toDo, completed: false})
+                this.toDo = ''
             }
         }
     })
