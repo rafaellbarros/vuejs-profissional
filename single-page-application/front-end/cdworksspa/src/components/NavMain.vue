ee6625486277db19a043a1973176da9dca761364
Rafael Barros <rafaelbarros.df@gmail.com>
2021-07-23 13:09:57 -0300

    :construction: Criando componentes de navegação e rodapé

---
+++ single-page-application/front-end/cdworksspa/src/components/NavMain.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand @click="$router.push('/')">CDWorks</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item @click="$router.push('/dashboard')"
+            >Sou Freelancer</b-nav-item
+          >
+          <b-nav-item @click="$router.push('/new-job')"
+            >Busco Freelancer</b-nav-item
+          >
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item @click="$router.push('/register')"
+            >Cadastrar-se</b-nav-item
+          >
+          <b-nav-item @click="$router.push('/login')">Entrar</b-nav-item>
+          <b-nav-item-dropdown right>
+            <template v-slot:button-content>
+              <em>NOME_USUARIO</em>
+            </template>
+            <b-dropdown-item @click="$router.push('/profile/1')"
+              >Perfil</b-dropdown-item
+            >
+            <b-dropdown-item @click="logout()">Sair</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      console.log("Saiu da aplicação!");
+    },
+  },
+};
+</script>
+
+<style></style>
