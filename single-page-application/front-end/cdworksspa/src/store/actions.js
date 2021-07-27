0776dabe5341efd168980ee1ce23d9c0bde44aaf
Rafael Barros <rafaelbarros.df@gmail.com>
2021-07-27 14:02:01 -0300

    :construction: Storage com Vuex

---
+++ single-page-application/front-end/cdworksspa/src/store/actions.js
@@ -0,0 +1,13 @@
+export default {
+    login(context, { user, token }) {
+        context.commit('setUser', user)
+        context.commit('setToken', token)
+        context.commit('setAuthenticated', true)
+    },
+
+    logout(context) {
+        context.commit('setUser', {})
+        context.commit('setToken', null)
+        context.commit('setAuthenticated', false)
+    },
+}
--- single-page-application/front-end/cdworksspa/src/store/index.js
+++ single-page-application/front-end/cdworksspa/src/store/index.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
+
+import actions from './actions'
+import mutations from './mutations'
+import state from './state'
+
+const vuexPersist = new VuexPersist({
+    key: 'cd-works',
+    storage: window.localStorage,
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
+    plugin: [vuexPersist.plugin],
+    state: state,
+    mutations: mutations,
+    actions: actions,
+    getters: {},
     modules: {},
 })
